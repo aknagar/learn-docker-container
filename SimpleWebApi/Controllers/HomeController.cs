9f788a621a6ec89c75aa7217956caf30fcfa64eb
aknagar <>
2018-01-28 14:57:43 +0530

    Initial Commit

---
+++ SimpleWebApi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleWebApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return this.Ok("Hi, this is a simple Web API which is to be containerized...Happy Containerizing");
+        }
+    }
+}
